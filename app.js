2d2aa9e605a0c10782fc86cca7eb91242b7d2b35
gary909 <garywhite61@hotmail.com>
2023-02-27 12:34:23 +0100

    code

---
+++ app.js
@@ -0,0 +1,9 @@
+function aliasGen(){
+    // Code Here
+}
+
+console.log(aliasGen("Mike", "Millington")); // "Malware Mike"
+console.log(aliasGen("Fahima", "Tash")); // "Function T-Rex"
+console.log(aliasGen("Daisy", "Petrovic")); // "Data Payload"
+console.log(aliasGen("7393424", "Anumbha")); // "Your name must start with a letter from A - Z."
+console.log(aliasGen("Anuddanumbha", "23200")); // "Your name must start with a letter from A - Z."
